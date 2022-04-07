765ec9327854c5475b0db40b1b9c8a6f41e53f6e
Kayode Taiwo <kay_taylor@outlook.com>
2022-04-07 15:25:16 -0500

    v4-commit

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,15 +1,50 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+//import { BankService } from 'src/app/bank.service';
+import { SnackbarService } from 'src/app/shared/snackbar.service';
+import { finalize } from 'rxjs/operators';
 
 @Component({
-  selector: 'app-dashboard',
-  templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.css']
+    selector: 'app-dashboard',
+    templateUrl: './dashboard.component.html',
+    styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent implements OnInit {
 
-  constructor() { }
+    balance: number = 0.0;
+    balanceLoading: boolean = false;
 
-  ngOnInit(): void {
-  }
+    depositForm: FormGroup;
+    withdrawForm: FormGroup;
+
+    isDepositing: boolean = false;
+    isWithdrawing: boolean = false;
+
+    constructor(
+        //public bankService: BankService,
+        private snackbarService: SnackbarService,
+    ) {
+        this.depositForm = new FormGroup({
+            depositAmount: new FormControl('', {
+                validators: [Validators.required, Validators.min(1), Validators.max(50000)],
+                updateOn: 'blur'
+            }),
+        });
+        this.withdrawForm = new FormGroup({
+            withdrawAmount: new FormControl('', {
+                validators: [Validators.required, Validators.min(1), Validators.max(50000)],
+                updateOn: 'blur'
+            }),
+        });
+    }
+
+    get depositAmount() { return this.depositForm.get('depositAmount'); }
+    get withdrawAmount() { return this.withdrawForm.get('withdrawAmount'); }
+
+    ngOnInit(): void {
+    }
+
+    onSubmitDeposit() { }
+    onSubmitWithdraw() { }
 
 }
