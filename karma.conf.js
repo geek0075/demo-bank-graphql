c541b8939ea022b779cf93637533d2264bfea0d3
Kay Taiwo <kay_taylor@outlook.com>
2022-04-12 23:20:09 +0000

    Update karma.conf.js

--- karma.conf.js
+++ karma.conf.js
@@ -1,44 +1,57 @@
-// Karma configuration file, see link for more information
-// https://karma-runner.github.io/1.0/config/configuration-file.html
+## Kayode Taiwo
 
-module.exports = function (config) {
-  config.set({
-    basePath: '',
-    frameworks: ['jasmine', '@angular-devkit/build-angular'],
-    plugins: [
-      require('karma-jasmine'),
-      require('karma-chrome-launcher'),
-      require('karma-jasmine-html-reporter'),
-      require('karma-coverage'),
-      require('@angular-devkit/build-angular/plugins/karma')
-    ],
-    client: {
-      jasmine: {
-        // you can add configuration options for Jasmine here
-        // the possible options are listed at https://jasmine.github.io/api/edge/Configuration.html
-        // for example, you can disable the random execution with `random: false`
-        // or set a specific seed with `seed: 4321`
-      },
-      clearContext: false // leave Jasmine Spec Runner output visible in browser
-    },
-    jasmineHtmlReporter: {
-      suppressAll: true // removes the duplicated traces
-    },
-    coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/vm-bank'),
-      subdir: '.',
-      reporters: [
-        { type: 'html' },
-        { type: 'text-summary' }
-      ]
-    },
-    reporters: ['progress', 'kjhtml'],
-    port: 9876,
-    colors: true,
-    logLevel: config.LOG_INFO,
-    autoWatch: true,
-    browsers: ['Chrome'],
-    singleRun: false,
-    restartOnFileChange: true
-  });
-};
+Fictitious Banking Application GraphQL. This application was generated with the Angular CLI and is an Angular (www.angular.io) + GraphQL application that has been deployed to the following url on Heroku:
+
+https://vbg-gql.herokuapp.com/
+
+## User accounts
+
+phone:0123211111, password:theophilus
+
+phone:0712340987, password:kayode
+
+Or create new accounts on the web site via user registration.
+
+## How to run the App on Heroku
+
+01. Navigate to https://vbg-gql.herokuapp.com/
+02. You will be automatically redirected to https://vbg-gql.herokuapp.com/home
+03. On the top menu bar click on Login / Register
+04. You will be redirected to the Login (https://vbg-gql.herokuapp.com/login) or Register (https://vbg-gql.herokuapp.com/register) page.
+05. To Register a new user fill the form on the Register page and click Register button.
+06. To Login fill the form on the Login page with your username (phone) and password.
+07. For example Login with these credentials: phone:0712340987, password:kayode.
+08. On thw top menu bar click on Dashboard.
+09. You will be redirected to your Dashboard: https://vbg-gql.herokuapp.com/dashboard
+10. Here you see your balance and history of transactions in descending order by date.
+11. You can use the UI for deposit and withdraw to make deposits and withdrawals.
+
+## How to run the App locally from Source
+
+01. Clone the app to a folder on your local computer, say vbg-gql.
+02. Navigate to the local folder by running, $ cd vbg-gql.
+03. Run the following command, $ npm install.
+04. Run the following command, $ npm run build.
+05. Run the following command, $ npm run start.
+06. You can now open http://localhost:3000 on your browser. The port to use will be displayed from previous step results.
+07. Proceed as described above in 'How to run the App on Heroku'.
+
+## Application backend dependency
+
+This application depends on a NestJS GraphQL API that is hosted on Heroku at the following url address:
+
+https://vbag.herokuapp.com
+
+With GraphQL Playground here:
+
+https://vbag.herokuapp.com/graphql
+
+So you need to make sure that the back end API is running in order to run the front end application. The back end API source code is available on gitlab at:
+
+https://gitlab.com/geek0075/veegil-bank-api-graphql
+
+## Stay in touch
+
+Kayode Taiwo - kay_taylor@outlook.com.
+
+Please send your comments and questions to me at the email above. Thank you and God bless.
